5e788d7d5a5d149324ddf0a024baaef3b5c28722
joy <guabaaa@2digit.io>
2024-02-28 17:39:45 +0900

    [Feat]메인페이지 화면개발

--- wikipage/src/components/MainSection.jsx
+++ wikipage/src/components/MainSection.jsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 const MainSection = () => {
     return (
-        <div>
-            
+        <div className='main_section_wrap'>
+            <div className='section_keyword_wrap'>
+                <span className='fw_bold fs_35'>키워드</span>
+                <div className='btn_wrap fw_500 fs_17'>
+                    <button>수정내역</button>
+                    <button>수정</button>
+                </div>
+            </div>
+            <div className='index_wrap'>
+                <span className='fw_500 fs_24'>목차</span>
+                <ol className='fs_17'>
+                    <li>데이터들어올 항목1</li>
+                    <li>데이터들어올 항목2</li>
+                    <li>데이터들어올 항목3</li>
+                </ol>
+            </div>
+            <div className='index_section_wrap'>
+                <span className='fw_500 fs_24'>1. 키워드데이터</span>
+                <div className='index_section'>블라블라 내용이 들어올 자리</div>
+            </div>
+            <div className='index_section_wrap'>
+                <span className='fw_500 fs_24'>2. 키워드데이터</span>
+                <div className='index_section'>블라블라 내용이 들어올 자리</div>
+            </div>
+            <div className='index_section_wrap'>
+                <span className='fw_500 fs_24'>3. 키워드데이터</span>
+                <div className='index_section'>블라블라 내용이 들어올 자리</div>
+            </div>
         </div>
     );
 };
--- wikipage/src/page/MainPage.jsx
+++ wikipage/src/page/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../assets/images/KakaoTalk_20240227_130259911.png'
 import {MdOutlineSearch} from 'react-icons/md'
+import MainSection from "../components/MainSection";
 
 const MainPage = () => {
     /** 검색 관련 함수 */
@@ -20,14 +21,26 @@ const MainPage = () => {
                     <h1 className='fw_bold fs_35 header_msg'>동박 위키 : </h1>
                     <h3 className='fw_500 fs_20'>어서오세요- 동박위키 입니다.</h3>
                     <div className='header_btm_wrap'>
-                        <img alt='logo' src={logo} width={250} />
+                        <img alt='logo' src={logo} width={250} style={{marginRight: '20px'}}/>
                         <div className='search_wrap'>
-                            <MdOutlineSearch />
-                            <input type='text' placeholder='검색' onKeyPress={handleSearch} />
+                            <MdOutlineSearch width={50} height={50} style={{verticalAlign:'middle'}} />
+                            <div style={{height: '50px'}}>
+                                <input style={{width: '80%'}} type='text' placeholder='검색' onKeyPress={handleSearch}/>
+                            </div>
                         </div>
-                        <button className='fw_500 fs_20' onClick={handleRandomPage}>Random</button>
+                        <div style={{height: '50px'}}>
+                            <button className='fw_500 fs_20' onClick={handleRandomPage}>Random</button>
+                        </div>
+                    </div>
+                    <div className='keyword_wrap fs_15 fw_bold'>
+                        <button>키워드1</button>
+                        <button>키워드2</button>
+                        <button>키워드3</button>
+                        <button>키워드4</button>
+                        <button>키워드5</button>
                     </div>
                 </div>
+                <MainSection />
             </div>
         </>
     );
